c750563fcd3fb3bbdfded43ac3671630b21a60bf
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-06-02 20:16:40 -0500

    I lost my load times

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
